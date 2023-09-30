0ef2eb0ce9be0c5ade4bb760a4df277d828f6d30
Adarsh Kumar <contactadarshk@gmail.com>
2023-09-30 12:00:21 +0400

    add comments in app.go

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,3 +1,4 @@
+// Package app handles the main application logic.
 package app
 
 import (
@@ -13,24 +14,27 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// application struct holds the app configuration.
 type application struct {
 	host   string
 	port   string
 	routes *chi.Mux
 }
 
+// readConfigs reads the configuration file and returns a Config struct.
 func readConfigs() *config.Config {
 	config, err := config.Read("./config.yml")
 	if err != nil {
-		log.Fatal().Err(err)
+		log.Fatal().Err(err).Msg("Failed to read config")
 	}
 
 	return config
 }
 
+// createServices initializes and returns instances of service layers.
 func createServices(conf *config.Config) (*auth.Service, *group.Service, *users.Service) {
 	if conf == nil {
-		log.Fatal().Msg("Conf is nil")
+		log.Fatal().Msg("Configuration is nil")
 	}
 
 	authService := auth.New(conf.Jwt)
@@ -40,28 +44,32 @@ func createServices(conf *config.Config) (*auth.Service, *group.Service, *users.
 	return authService, groupService, usersService
 }
 
+// new initializes the application and returns its instance.
 func new() *application {
 	config := readConfigs()
 
 	authService, groupService, usersService := createServices(config)
 	routes := createRoutes(authService, groupService, usersService)
 
-	app := application{
+	return &application{
 		host:   config.Server.Host,
 		port:   config.Server.Port,
 		routes: routes,
 	}
-
-	return &app
 }
 
+// Run starts the application.
 func Run() {
+	// Set logging time format
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
+	// Initialize the application
 	app := new()
 
+	// Construct the address and start the HTTP server
 	addr := fmt.Sprintf("%s:%s", app.host, app.port)
 	log.Info().Msg(fmt.Sprintf("Running application at %s", addr))
-	err := http.ListenAndServe(addr, app.routes)
-	log.Fatal().Err(err).Msg("Crashed")
+	if err := http.ListenAndServe(addr, app.routes); err != nil {
+		log.Fatal().Err(err).Msg("Application crashed")
+	}
 }
